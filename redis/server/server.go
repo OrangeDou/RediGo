b4b9f5ea95337d96b8ff8c85f1a86c6482a90258
文强 <wenqiangsspu@163.com>
2024-08-12 14:51:49 +0800

    add: redis server

---
+++ redis/server/server.go
@@ -0,0 +1,107 @@
+package server
+
+import (
+	"context"
+	"io"
+	"net"
+	"strings"
+	"sync"
+
+	"redigo/lib/logger"
+	"redigo/lib/sync/atomic"
+	"redigo/redis/protocol"
+
+	database "redigo/interface/datebase"
+
+	"github.com/hdt3213/godis/redis/connection"
+	"github.com/hdt3213/godis/redis/parser"
+)
+
+/*
+ * A tcp.Handler implements redis protocol
+ */
+var (
+	unknownErrReplyBytes = []byte("-ERR unknown\r\n")
+)
+
+// Handler implements tcp.Handler and serves as a redis server
+type Handler struct {
+	activeConn sync.Map // *client -> placeholder
+	db         database.DB
+	closing    atomic.Boolean // refusing new client and new request
+}
+
+// MakeHandler creates a Handler instance
+func MakeHandler() *Handler {
+	var db database.DB
+	return &Handler{
+		db: db,
+	}
+}
+func (h *Handler) closeClient(client *connection.Connection) {
+	_ = client.Close()
+	h.db.AfterClientClose(client)
+	h.activeConn.Delete(client)
+}
+
+func (h *Handler) Handler(ctx context.Context, conn net.Conn) {
+	if h.closing.Get() {
+		_ = conn.Close()
+		return
+	}
+
+	client := connection.NewConn(conn)
+	h.activeConn.Store(client, struct{}{})
+	ch := parser.ParseStream(conn)
+	for payload := range ch {
+		if payload.Err != nil {
+			if payload.Err == io.EOF ||
+				payload.Err == io.ErrUnexpectedEOF ||
+				strings.Contains(payload.Err.Error(), "use of closed network connection") {
+				// connection closed
+				h.closeClient(client)
+				//logger.Info("connection closed: " + client.RemoteAddr())
+				return
+			}
+			// protocol err
+			errReply := protocol.MakeErrReply(payload.Err.Error())
+			_, err := client.Write(errReply.ToBytes())
+			if err != nil {
+				h.closeClient(client)
+				//logger.Info("connection closed: " + client.RemoteAddr())
+				return
+			}
+			continue
+		}
+		if payload.Data == nil {
+			logger.Error("empty payload")
+			continue
+		}
+		r, ok := payload.Data.(*protocol.MultiBulkReply)
+		if !ok {
+			logger.Error("require multi bulk protocol")
+			continue
+		}
+		result := h.db.Exec(client, r.Args)
+		if result != nil {
+			_, _ = client.Write(result.ToBytes())
+		} else {
+			_, _ = client.Write(unknownErrReplyBytes)
+		}
+	}
+
+}
+
+// Close stops handler
+func (h *Handler) Close() error {
+	logger.Info("handler shutting down...")
+	h.closing.Set(true)
+	// TODO: concurrent wait
+	h.activeConn.Range(func(key interface{}, val interface{}) bool {
+		client := key.(*connection.Connection)
+		_ = client.Close()
+		return true
+	})
+	h.db.Close()
+	return nil
+}
