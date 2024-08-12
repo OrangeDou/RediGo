93ad50e70ebbb0b9409db24709947de13ed244c9
文强 <wenqiangsspu@163.com>
2024-08-12 15:05:03 +0800

    add: main

--- main.go
+++ main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/spf13/viper"
+	"redigo/config"
+	"redigo/lib/logger"
+	"redigo/lib/utils"
+	RedisServer "redigo/redis/server"
+	"redigo/tcp"
 )
 
-func main() {
+var banner = `
+   ______          ___
+  / ____/___  ____/ (_)____
+ / / __/ __ \/ __  / / ___/
+/ /_/ / /_/ / /_/ / (__  )
+\____/\____/\__,_/_/____/
+`
 
+var defaultProperties = &config.ServerProperties{
+	Bind:           "0.0.0.0",
+	Port:           6399,
+	AppendOnly:     false,
+	AppendFilename: "",
+	MaxClients:     1000,
+	RunID:          utils.RandString(40),
 }
 
-func Init() {
-	viper.SetConfigFile("config/config.yaml")
-	err := viper.ReadInConfig() // 查找并读取配置文件
-	if err != nil {             // 处理读取配置文件的错误
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
+
+func main() {
+	print(banner)
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "godis",
+		Ext:        "log",
+		TimeFormat: "2006-01-02",
+	})
+	configFilename := os.Getenv("CONFIG")
+	if configFilename == "" {
+		if fileExists("redis.conf") {
+			config.SetupConfig("redis.conf")
+		} else {
+			config.Properties = defaultProperties
+		}
+	} else {
+		config.SetupConfig(configFilename)
+	}
+	err := tcp.ListenAndServeWithSignal(&tcp.Config{
+		Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+	}, RedisServer.MakeHandler())
+	if err != nil {
+		logger.Error(err)
 	}
 }
